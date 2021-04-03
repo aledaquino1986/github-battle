2b74c5c895debbc886055774fae8aa8832e5b136
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-04-03 13:47:20 -0300

    Adding state to navbar. Changes styles based on selected state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 
 class Popular extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(language) {
+    this.setState({
+      selectedLanguage: language
+    });
+  }
   render() {
     const languages = ["All", "Javascript", "Ruby", "CSS", "Python", "Java"];
     return (
@@ -8,7 +22,17 @@ class Popular extends Component {
         {languages.map(language => {
           return (
             <li key={language}>
-              <button className="btn-clear nav-link">{language} </button>
+              <button
+                style={
+                  language === this.state.selectedLanguage
+                    ? { color: "rgb(187, 42, 31" }
+                    : null
+                }
+                onClick={() => this.updateLanguage(language)}
+                className="btn-clear nav-link"
+              >
+                {language}{" "}
+              </button>
             </li>
           );
         })}
