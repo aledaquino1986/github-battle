5c08c5c51aadfb310d1b508e301959cdb59b04f4
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-05-24 10:00:12 -0300

    refactor card and nav to use useContext

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,47 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import { ThemeConsumer } from "../contexts/theme";
+import ThemeContext from "../contexts/theme";
 
 const activeStyle = {
   color: "rgb(187, 46, 31)"
 };
 
 export default function Nav({ toggleTheme }) {
+  const theme = React.useContext(ThemeContext);
   return (
-    <ThemeConsumer>
-      {theme => (
-        <nav className="row space-between">
-          <ul className="row nav">
-            <li>
-              <NavLink
-                exact
-                to="/"
-                className="nav-link"
-                activeStyle={activeStyle}
-              >
-                Popular
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/battle"
-                className="nav-link"
-                activeStyle={activeStyle}
-              >
-                Battle
-              </NavLink>
-            </li>
-          </ul>
-          <button
-            style={{ fontSize: 30 }}
-            className="btn-clear"
-            onClick={toggleTheme}
-          >
-            {theme === "light" ? "🔦" : "💡"}
-          </button>
-        </nav>
-      )}
-    </ThemeConsumer>
+    <nav className="row space-between">
+      <ul className="row nav">
+        <li>
+          <NavLink exact to="/" className="nav-link" activeStyle={activeStyle}>
+            Popular
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/battle" className="nav-link" activeStyle={activeStyle}>
+            Battle
+          </NavLink>
+        </li>
+      </ul>
+      <button
+        style={{ fontSize: 30 }}
+        className="btn-clear"
+        onClick={toggleTheme}
+      >
+        {theme === "light" ? "🔦" : "💡"}
+      </button>
+    </nav>
   );
 }
